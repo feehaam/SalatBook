7c4f77b9775318a3d9011f9f2c31b744e3151d36
feehaam <mdfeeham@gmail.com>
2023-01-08 05:09:15 +0600

    Initial setup

---
+++ Back end/Application/DTO/StudentDto.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Model;
+
+namespace ApplicationLayer.DTO
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
---
+++ Back end/Application/Map/MappintProfiles.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationLayer.DTO;
+using AutoMapper;
+using DataAccessLayer.Model;
+
+namespace ApplicationLayer.Map
+{
+    public class MappintProfiles : Profile
+    {
+        public MappintProfiles()
+        {
+            CreateMap<StudentDto, Student>();
+            CreateMap<CourseDto, Course>();
+        }
+    }
+}
