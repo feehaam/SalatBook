8dbff42606d2c29e0d83b1e7dc9e0eaac88ac690
feehaam <mdfeeham@gmail.com>
2023-01-16 01:00:46 +0600

    User contoller implemented

---
+++ Back end/Application/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using DataAccessLayer.IRepo;
+using DataAccessLayer.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUser userDLL;
+
+        public UserController(IUser userDLL)
+        {
+            this.userDLL = userDLL;
+        }
+
+        [HttpPost("/create_user/")]
+        public IActionResult CreateUser(User user)
+        {
+            try
+            {
+                if (!userDLL.CreateUser(user))
+                {
+                    return BadRequest("Duplicate or null! Could not create new entity.");
+                }
+                return Ok("Creation succesfull!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error while creating new user! --> " + e.Message);
+            }
+        }
+        [HttpGet("/read_user{id}")]
+        public IActionResult ReadUser(int id)
+        {
+            try
+            {
+                if(!userDLL.UserExists(id))
+                {
+                    return NotFound();
+                }
+                var user = userDLL.ReadUser(id);
+                if (user == null) return BadRequest("Student doesn't exist, Or fail to parse!");
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return NotFound("Error while seraching user! --> " + e.Message);
+            }
+        }
+        [HttpGet("/find_user{emailOrUserName}")]
+        public IActionResult FindUser(string emailOrUsername)
+        {
+            try
+            {
+                var user = userDLL.FindUser(emailOrUsername);
+                if (user == null) return BadRequest("Student doesn't exist, Or fail to parse!");
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return NotFound("Error while seraching user! --> " + e.Message);
+            }
+        }
+        [HttpPut("/update_user")]
+        public IActionResult UpdateUser(User user)
+        {
+            try
+            {
+                if(user != null)
+                if (!userDLL.UpdateUser(user))
+                {
+                    return BadRequest("Error while updating the user");
+                }
+                return Ok("Update succesfull");
+            }
+            catch (Exception e)
+            {
+                return NotFound("Error while updating user! --> " + e.Message);
+            }
+        }
+        
+        [HttpDelete("/delete_user")]
+        public IActionResult DeleteUser(int id)
+        {
+            try
+            {
+                if (!userDLL.UserExists(id)){
+                    return NotFound();
+                }
+                if (!userDLL.DeleteUser(id))
+                {
+                    return BadRequest("Error while deleting the user");
+                }
+                return Ok("Deleted.");
+            }
+            catch (Exception e)
+            {
+                return NotFound("Error while deleting user! --> " + e.Message);
+            }
+        }
+    }
+}
